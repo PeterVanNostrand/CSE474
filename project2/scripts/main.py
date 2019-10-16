d43c6722b940a7a8c0c8871a88845d9956c37146
PeterVanNostrand <petermvannostrand@gmail.com>
2019-10-16 00:42:21 -0400

    trying sgd

--- project2/scripts/main.py
+++ project2/scripts/main.py
@@ -24,7 +24,6 @@ def relu(z):
 #     return 1 / (1 + np.exp(-z))
 
 def softmax(z):
-    """Compute softmax values for each sets of scores in x."""
     ez = np.exp(z - np.max(z, axis=0))
     return ez / np.sum(ez, axis=0)
 
@@ -35,7 +34,7 @@ def softmax(z):
 
 def cost(a, y):
     diff = (a-y)
-    loss = np.square(diff)
+    loss = np.square(diff)  
     # loss = np.multiply(y, np.log(a)) # ylog(a)
     return np.sum(loss, axis=0) # determine the loss for each of the 10 output neurons
 
@@ -77,7 +76,9 @@ class layer_hidden:  # Layer 1: Hidden Layer - Fully connected
         # self.del_w = np.dot(del_z, self.prev_layer.a.T)
 
         del_a = np.dot((next_layer.a - y).T, next_layer.w).T # 392 x 60k
-        del_z = np.max(del_a.copy(), 0) # 392 x 60k
+        del_z = del_a.copy() # 392 x 60k
+        del_z[del_z < 0] = 0
+        del_z[del_z > 0] = 1
         # del_z1[h < 0] = 0 # set all neg value to 0
         self.del_w = np.dot(self.prev_layer.a, del_z.T).T
 
@@ -149,37 +150,33 @@ if __name__ == '__main__':
 
     loss = []
 
-    batch_size = 24
-    num_epochs = 25
-    for j in range (0, num_epochs):
-        for i in range(0, int(ns/batch_size)):
-            batch_ins = x_train[i*batch_size:(i+1)*batch_size,:]
-            batch_labels = y.T[i*batch_size:(i+1)*batch_size,:].T
-            l0.set_inputs(batch_ins)
+    for i in range(0, ns):
+        batch_y = y.T[i].reshape((10,1))
+        l0.set_inputs(x_train[i].reshape((1,784)))
 
-            # forwared propagate
-            l1.forward()
-            l2.forward()
+        # forwared propagate
+        l1.forward()
+        l2.forward()
 
-            # compute cost
-            cost_val = np.mean(cost(l2.a, batch_labels))
-            print(cost_val)
-            loss.append(cost_val)
+        # compute cost
+        cost_val = np.mean(cost(l2.a, batch_y))
+        print(cost_val)
+        loss.append(cost_val)
 
-            # back propagate and update weights
-            l2.backward(batch_labels)
-            l1.backward(batch_labels, l2)
+        # back propagate and update weights
+        l2.backward(batch_y)
+        l1.backward(batch_y, l2)
 
-            l2.update(0.5)
-            l1.update(0.5)
+        l2.update(0.01)
+        l1.update(0.01)
 
-            # randomly reorder the training data (rebuilding labels to correspond)
-            # data = np.hstack((x_train, y_train.reshape(60000, 1)))
-            # np.random.shuffle(data)
-            # x_train = data[:,:-1]
-            # y_train = data[:,-1:].reshape(60000)
-            # for i in range(0, ns): # set desired output to 1 for correct class, 0 for rest
-            #     y[int(y_train[i])][i] = 1
+        # randomly reorder the training data (rebuilding labels to correspond)
+        # data = np.hstack((x_train, y_train.reshape(60000, 1)))
+        # np.random.shuffle(data)
+        # x_train = data[:,:-1]
+        # y_train = data[:,-1:].reshape(60000)
+        # for i in range(0, ns): # set desired output to 1 for correct class, 0 for rest
+        #     y[int(y_train[i])][i] = 1
 
     plt.figure()
     plt.plot(loss)
